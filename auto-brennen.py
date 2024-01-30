2683c3e39e11f89012174ceb5cd2967e1de9a454
jack norris <jacktimothynorris@gmail.com>
2024-01-30 13:06:10 -0600

    updated readme and epoch measurement

--- auto-brennen.py
+++ auto-brennen.py
@@ -123,7 +123,7 @@ def train_model(home_dir, model_output_destination, concepts_loc, num_epochs):
     train_commands['workspace-dir'] = home_dir + '/workspace/run'
     train_commands['cache-dir'] = home_dir + '/workspace-cache/run'
     train_commands['concept-file-name'] = concepts_loc
-    train_commands['epochs'] = 10
+    train_commands['epochs'] = num_epochs
     train_commands['batch-size'] = batch_size
     train_commands['output-model-destination'] = model_output_destination
     subprocess.run("python scripts/train.py " + dict_to_command_line_args(train_commands) + " --tensorboard --gradient-checkpointing --backup-before-save --aspect-ratio-bucketing --latent-caching --clear-cache-before-training --train-unet --train-prior --samples-to-tensorboard --non-ema-sampling --backup-before-save", shell=True)
--- jack-readme.md
+++ jack-readme.md
@@ -2,6 +2,8 @@
 
  python .\auto-brennen.py --input_directory=<INPUT_DIRECTORY>
 
- As an example, the imput_directory could be : E:\B\Pozy\Downloaded_Folder\Collage
+ As an example, the imput_directory could be : E:\B\Pozy\Downloaded_Folder\Collage. For this, for every folder in the collage folder, you are creating a model
 
  If you are dealing with PNGs, make sure the "sample-image-format" value in the arguments.json file is PNG. Otherwise, use JPG
+
+ Files are "crawled" via their last edit, so feel free to go into your folder of choice and sort by date
